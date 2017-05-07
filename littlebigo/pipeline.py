392b86c9a451d08a479271f4652481878f4d0cf9
Hannes Holste <hannes.holste@gmail.com>
2017-05-07 11:17:40 -0700

    Add test for simple subsampling pipeline

--- littlebigo/pipeline.py
+++ littlebigo/pipeline.py
@@ -9,11 +9,17 @@ class Pipeline:
     def execute(self):
         print('Running pipeline: "{}"'.format(self.name))
 
+        prev_return_vals = None
         # Execute each step
         for (func, desc) in self.steps:
             # TODO: support wrapping certain steps in runtime environments like
             # supercomputer instead of executing directly
             print('- Executing step: "{}"'.format(self.name))
-            func()
+
+            if prev_return_vals is not None:
+                prev_return_vals = func(prev_return_vals)
+            else:
+                prev_return_vals = func()
+
 
     # TODO: support waiting for supercomputer output
--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -1,6 +1,12 @@
-import pytest
+import os
+from subprocess import call
+
+from skbio import DistanceMatrix
+
 from littlebigo import Dataset
 from littlebigo import Pipeline
+
+from mdsa import subsample
 
 
 def test_pipeline():
@@ -12,3 +18,45 @@ def test_pipeline():
     p.add(load_emp_dataset, 'Load test dataset')
 
     p.execute()
+
+
+def test_emp_subsample_pipeline():
+    OUT_PATH = './outputs/test-dataset-subsampled.txt'
+    SUBSAMPLE_DIM = 10
+
+    # Delete output file if exists to 'reset' test
+    if os.path.exists(OUT_PATH):
+        os.remove(OUT_PATH)
+
+    def load_emp_dataset():
+        data = Dataset('test-dataset',
+                       './tests/test-dataset.txt')
+        return data
+
+    def subsamp(dataset):
+        input_matrix = DistanceMatrix.read(dataset.filepath, format='lsmat')
+        subsampled_matrix = subsample.subsample(input_matrix, SUBSAMPLE_DIM)
+        return subsampled_matrix
+
+    def save(subsampled_matrix):
+        subsampled_matrix.write(OUT_PATH)
+
+        # Track metadata (output file location)
+        out_data = Dataset({
+            'name': 'emp',
+            'subsampled': True,
+            'dimension': SUBSAMPLE_DIM
+        }, OUT_PATH)
+
+    p = Pipeline('test-subsample')
+    # load EMP dataset
+    p.add(load_emp_dataset, 'Load test dataset')
+
+    # send to subsampler
+    p.add(subsamp, 'Subsample dataset')
+
+    p.add(save, 'Save subsampled matrix to disk')
+
+    p.execute()
+
+    assert os.path.exists(OUT_PATH)
